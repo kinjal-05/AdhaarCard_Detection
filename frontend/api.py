1a3d8ef05148ada1a931efcede6336ef316f3dec
Kinjal Mistry <165459555+kinjal-05@users.noreply.github.com>
2025-03-23 13:07:39 +0530

    Update api.py

--- frontend/api.py
+++ frontend/api.py
@@ -109,4 +109,5 @@ def extract_aadhaar_api():
     return jsonify({"error": "Aadhaar number could not be extracted."}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000) 
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port) 
